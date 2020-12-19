20787d00c1b3d573a54eb97ca42f037a5a02acdb
JWaters02 <44036776+JWaters02@users.noreply.github.com>
2020-12-19 04:30:51 +0000

    Toggling/saving theme works

--- Main/Scripts/getTheme.js
+++ Main/Scripts/getTheme.js
@@ -1,6 +1,33 @@
-const theme = document.querySelector("#theme-link");
-if (window.localStorage.getItem("theme") === "dark") {
-    theme.href = "./CSS/light-theme.css";
-} else if (window.localStorage.getItem("theme") === "light") {
-    theme.href = "./CSS/dark-theme.css";
-}
+const onThemeUpdated = (theme) => {
+	console.log('Updated theme to: ${theme}');
+    const themes = document.querySelector("#theme-link");
+    if (theme === 'dark') {
+        themes.href = "./CSS/light-theme.css"
+    } else {
+        themes.href = "./CSS/dark-theme.css"
+    }
+    const rootElement = document.getElementById('root');
+    rootElement.className = theme;
+};
+const updateTheme = (theme) => {
+    if (theme !== 'dark' && theme !== 'light') {
+        console.error('Tried to update theme, but did not recognise ${theme} theme.');
+        return;
+    }
+    window.localStorage.setItem('theme', theme);
+    onThemeUpdated(theme);
+};
+const getTheme = () => window.localStorage.getItem('theme');
+const toggleTheme = () => {
+	const theme = getTheme();
+    const updatedTheme = theme === 'dark' ? 
+        'light' : 
+        'dark';
+        
+    updateTheme(updatedTheme);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	const theme = getTheme();
+    updateTheme(theme);
+});
